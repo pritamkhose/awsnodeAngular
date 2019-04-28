21752fafd506bb44296b2aa144b69e06c4849b75
pritamkhose <pritamkhose@gmail.com>
2019-04-28 23:09:54 +0530

    Added PWA

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { SwUpdate } from '@angular/service-worker';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'awsnodeAngular';
+
+  constructor ( updates: SwUpdate) {
+    console.log('update app Logic -->');
+    updates.available.subscribe( event => {
+      updates.activateUpdate().then(() => document.location.reload());
+    });
+  }
 }
