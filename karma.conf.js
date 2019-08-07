c291fbf98da8414faccca7fa918c613bc1289fac
pritamkhose <pritamkhose@gmaio.com>
2019-08-08 02:50:09 +0530

    pwa update

--- karma.conf.js
+++ karma.conf.js
@@ -16,7 +16,7 @@ module.exports = function (config) {
       clearContext: false // leave Jasmine Spec Runner output visible in browser
     },
     coverageIstanbulReporter: {
-      dir: require('path').join(__dirname, '../coverage/awsnodeAngular'),
+      dir: require('path').join(__dirname, './coverage/awsnodeAngular'),
       reports: ['html', 'lcovonly', 'text-summary'],
       fixWebpackSourcePaths: true
     },
